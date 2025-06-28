d34f3f4b573ec0607f529ec1dd612acbdc2f3a08
namanr09 <rohitasnaman09@gmail.com>
2025-06-28 11:05:19 +0530

    update simpleBootloader with new boot.asm and boot.bin files

--- simpleBootloader/boot.asm
+++ simpleBootloader/boot.asm
@@ -6,18 +6,40 @@ BITS 16
 ; Use 16-bit mode   
 
 start:
+    mov si, message
+    call print
+    ; Print the message
+    jmp $
+
+print: 
+    mov bx, 0
+    ; Set BX to 0 (video memory)
+.loop:
+    lodsb 
+    ; Load the string address into SI
+    cmp al,0
+    je .done
+    ; Check if the character is null (end of string)
+    call print_char
+    ; Call the function to print the character
+    jmp .loop
+.done:
+    ret
+
+print_char:
     mov ah, 0eh 
     ; Function to read sectors
-    mov al, 'A' 
+    ; mov al, 'A' 
     ; Character to print
     mov bx, 0x0000 
-    ; Set BX to 0 (video memory)
     int 0x10 
     ; BIOS interrupt to print character
     ; thats why bios is like a kernel itself
+    ret
 
-    jmp $
-
+message:
+    db 'Hello, World!', 0x0D, 0x0A ; Message to print
+    db 'Bootloader loaded successfully!', 0x0D, 0x0A
 ; signature 
 times 510-($ - $$) db 0 ; Fill the rest of the boot sector with zeros
 dw 0xAA55 ; Boot sector signature
